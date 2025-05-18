997333a6d93e0c4c93d2459040c5bdaf7c3d8f1c
Mehedi Hasan <mh.majumdar1952@gmail.com>
2025-05-18 21:33:12 +0600

    Project Structure Created

---
+++ __init__.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_login import LoginManager
+from extensions import db
+
+DB_NAME = "database.db"
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = "helloworld"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
+   
+
+    # Register blueprints
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
+    app.register_blueprint(views, url_prefix="/", name="home")
+
+
+    # Create database
+    with app.app_context():
+        db.create_all()
+        print("Database created!")
+
+    return app
